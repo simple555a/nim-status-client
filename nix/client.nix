655a8df51c3217bebba766d7dc8d9f05e3ca789d
Jakub Sokołowski <jakub@status.im>
2020-08-13 14:02:44 +0200

    add building of DOtherSide
    Signed-off-by: Jakub Sokołowski <jakub@status.im>

--- nix/client.nix
+++ nix/client.nix
@@ -24,14 +24,14 @@ in stdenv.mkDerivation {
         "src/.*" "vendor/.*"
       ];
       exclude = [
-        "vendor/.nimble/.*" ".*/vendor/status-go/.*"
+        "vendor/.nimble/.*" ".*vendor/status-go/.*"
         ".*/nimbus-build-system/vendor/.*"
       ];
     };
   };
   
-  buildInputs = with pkgs; [ nim git pkgconfig which ];
-  propagatedBuildInputs = with pkgs; [ openssl pcre qtCustom ];
+  buildInputs = with pkgs; [ nim git cmake pkgconfig which ];
+  propagatedBuildInputs = with pkgs; [ openssl pcre libGL qtCustom ];
 
   QTDIR = qtCustom;
   QT5_PCFILEDIR = "${qtCustom}/lib/pkgconfig";
@@ -46,17 +46,35 @@ in stdenv.mkDerivation {
     export PWD_CMD=$(which pwd)
     patchShebangs $NIMBLE_LINK_SCRIPT
     for dep_dir in $(find vendor -type d -maxdepth 1); do
-        pushd "$dep_dir"
+        pushd "$dep_dir" >/dev/null
         $NIMBLE_LINK_SCRIPT "$dep_dir"
-        popd
+        popd >/dev/null
     done
   '';
 
+  preBuildPhase = ''
+    set -x
+    mkdir -p vendor/DOtherSide/build
+    pushd vendor/DOtherSide/build
+    rm -f CMakeCache.txt
+    cmake \
+        -DENABLE_DYNAMIC_LIBS=ON \
+        -DENABLE_STATIC_LIBS=OFF \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DENABLE_DOCS=OFF \
+        -DENABLE_TESTS=OFF \
+        ..
+    cmake --build . --config Release
+    popd
+  '';
+
   buildPhase = ''
+    runHook preBuildPhase
     nim c ${concatStringsSep " " nimParams} \
+      --passL:"-L$QT5_LIBDIR" \
       --passL:"${status-go}/lib/libstatus.a" \
+      --passL:"vendor/DOtherSide/build/lib/libDOtherSideStatic.a" \
       --passL:"vendor/QR-Code-generator/c/libqrcodegen.a" \
-      --passL:"-lsetupapi -lhid" \
       --passL:"-lm" \
       --passL:"-lpcre" \
       -d:usePcreHeader \
